5bd6a150273cc045eafef343c7858e41bdfa0087
Stetson Gafford <StetsonG@users.noreply.github.com>
2022-08-05 22:51:20 -0400

    Change image on reload

--- app.js
+++ app.js
@@ -1,22 +1,62 @@
-import ColorThief from './node_modules/colorthief/dist/color-thief.mjs'
-
 const maxImage = 21;
-const randImageIndex = Math.floor(Math.random() * maxImage);
-const randImagePath = `images/${randImageIndex}.png`;
+// const randImageIndex = 1 + Math.floor(Math.random() * (maxImage - 1));
+// const randImagePath = `images/${randImageIndex}.jpeg`;
 
-console.log("Image selected: " + randImagePath);
+// Get image from cookie
+var imageIndex = getCookie("image");
+if (!imageIndex){
+    imageIndex = 1;
+}
+const imagePath = `images/${imageIndex}.jpeg`;
+console.log("Image selected: " + imagePath);
+
+let nextImage =  parseInt(imageIndex) + 1;
+if (nextImage > maxImage) { nextImage = 1 }
+
+// Set cookie for next load
+setCookie("image", nextImage);
 
 const img = document.querySelector('img.logo');
+const background = document.querySelector('div.background');
+const link = document.querySelector("link[rel='shortcut icon']");
 
-img.setAttribute('src', randImagePath);
-document.body.style.backgroundImage = "url('" + randImagePath + "')";
+// Cancel loading
+img.setAttribute('src', "");
+background.style.backgroundImage = "";
 
-const colorThief = new ColorThief();
+img.setAttribute('src', imagePath);
+background.style.backgroundImage = "url('" + imagePath + "')";
+link.href = imagePath;
 
 if (img.complete) {
-  colorThief.getColor(img);
+    updateTextColor(img);
 } else {
-  image.addEventListener('load', function() {
-    colorThief.getColor(img);
+  img.addEventListener('load', function() {
+    updateTextColor(img);
   });
+}
+
+function updateTextColor(img) {
+    const colorThief = new ColorThief();
+
+    // Extract the two most prominant colors from the image
+    var palette = colorThief.getPalette(img, 2);
+
+    console.log(palette);
+
+    // Set the text background as a gradient between those two colors
+    const textGradient = `linear-gradient(45deg, rgb(${palette[1][0]}, ${palette[1][1]}, ${palette[1][2]}), rgb(${palette[0][0]}, ${palette[0][1]}, ${palette[0][2]}))`
+
+    const text = document.querySelector('div.text');
+    text.style.backgroundImage = textGradient;
+}
+
+function setCookie(name, value){
+    document.cookie = `${name}=${value}`;
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
 }
